250e2fe49625b5f012fcccfae19367e2a33ab72e
stevewang <stevewang@easyops.cn>
2020-08-23 12:23:32 +0800

    test: wait for gatsby initial rendered

--- cypress/integration/color-mode.spec.js
+++ cypress/integration/color-mode.spec.js
@@ -3,7 +3,7 @@
 describe('Color Mode', () => {
   it('Toggle color mode', () => {
     cy.visit('/')
-    cy.document().its("fonts.status").should("equal", "loaded")
+    cy.waitForRouteChange()
     cy.get('body').should('have.css', 'backgroundColor', 'rgb(250, 250, 249)')
     cy.contains('header a[role="button"]', 'Dark').as("toggle").click()
     cy.get("@toggle").should("contain.text", "Light")
--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -3,8 +3,9 @@
 describe('Post', () => {
   it('Go to post page', () => {
     cy.visit('/')
-    cy.document().its("fonts.status").should("equal", "loaded")
+    cy.waitForRouteChange()
     cy.contains("main > article:last-of-type > header > h2 > a", "Markdown").click()
+    cy.waitForRouteChange()
     cy.location("pathname").should("equal", "/writing-articles-in-markdown/")
     cy.contains("main > article > header > h1", "Markdown")
   })
